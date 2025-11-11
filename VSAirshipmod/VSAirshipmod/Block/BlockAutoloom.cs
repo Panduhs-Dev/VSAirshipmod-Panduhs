23e0113bf86d076038045ab802d5cf2a89d7f32d
Rivvion <Rivvion@live.nl>
2025-11-11 09:52:30 +0100

    Autoloom and threadspinner 1st draft, no craft or sounds yet

--- VSAirshipmod/VSAirshipmod/Block/BlockAutoloom.cs
+++ VSAirshipmod/VSAirshipmod/Block/BlockAutoloom.cs
@@ -13,6 +13,7 @@ using Vintagestory.GameContent;
 using Vintagestory.GameContent.Mechanics;
 using VSAirshipmod.NSBlockEntity;
 
+
 namespace VSAirshipmod.NSBlock
 {
     public class BlockAutoloom : BlockMPBase
@@ -20,149 +21,87 @@ namespace VSAirshipmod.NSBlock
         public override bool TryPlaceBlock(IWorldAccessor world, IPlayer byPlayer, ItemStack itemstack, BlockSelection blockSel, ref string failureCode)
         {
             bool ok = base.TryPlaceBlock(world, byPlayer, itemstack, blockSel, ref failureCode);
+            if (!ok) return false;
 
-            if (ok)
+            BlockPos pos = blockSel.Position;
+            Block block = world.BlockAccessor.GetBlock(pos);
+
+            float rotY = block.Shape?.rotateY ?? 0f;
+            int rotSteps = ((int)Math.Round(rotY / 90f) % 4 + 4) % 4;
+
+            BlockFacing[] horizontals = BlockFacing.HORIZONTALS;
+            int index = Array.IndexOf(horizontals, BlockFacing.WEST);
+            int rotatedIndex = (index + rotSteps) % 4;
+
+            if (rotSteps == 1 || rotSteps == 3)
             {
-                if (!tryConnect(world, byPlayer, blockSel.Position, BlockFacing.UP))
-                {
-                    tryConnect(world, byPlayer, blockSel.Position, BlockFacing.DOWN);
-                }
+                rotatedIndex = (rotatedIndex + 2) % 4;
             }
 
-            return ok;
-        }
+            BlockFacing connectorFace = horizontals[rotatedIndex];
 
-        public override bool DoParticalSelection(IWorldAccessor world, BlockPos pos)
-        {
+            tryConnect(world, byPlayer, pos, connectorFace);
+
             return true;
         }
 
+
+
+
         public override bool OnBlockInteractStart(IWorldAccessor world, IPlayer byPlayer, BlockSelection blockSel)
         {
-            if (blockSel != null && !world.Claims.TryAccess(byPlayer, blockSel.Position, EnumBlockAccessFlags.Use))
-            {
+            if (blockSel == null || !world.Claims.TryAccess(byPlayer, blockSel.Position, EnumBlockAccessFlags.Use))
                 return false;
-            }
 
-            BlockEntityAutoloom beQuern = world.BlockAccessor.GetBlockEntity(blockSel.Position) as BlockEntityAutoloom;
+            var be = world.BlockAccessor.GetBlockEntity(blockSel.Position) as BlockEntityAutoloom;
+            if (be == null) return false;
 
-            if (beQuern != null && beQuern.CanGrind() && (blockSel.SelectionBoxIndex == 1 || beQuern.Inventory.openedByPlayerGUIds.Contains(byPlayer.PlayerUID)))
-            {
-                beQuern.SetPlayerGrinding(byPlayer, true);
+            // Let the BE handle GUI/client logic
+            if (be.OnPlayerRightClick(byPlayer, blockSel))
                 return true;
-            }
 
             return base.OnBlockInteractStart(world, byPlayer, blockSel);
         }
 
-        public override bool OnBlockInteractStep(float secondsUsed, IWorldAccessor world, IPlayer byPlayer, BlockSelection blockSel)
-        {
-            BlockEntityAutoloom beQuern = world.BlockAccessor.GetBlockEntity(blockSel.Position) as BlockEntityAutoloom;
 
-            if (beQuern != null && (blockSel.SelectionBoxIndex == 1 || beQuern.Inventory.openedByPlayerGUIds.Contains(byPlayer.PlayerUID)))
-            {
-                beQuern.IsGrinding(byPlayer);
-                return beQuern.CanGrind();
-            }
-
-            return false;
-        }
-
-        public override void OnBlockInteractStop(float secondsUsed, IWorldAccessor world, IPlayer byPlayer, BlockSelection blockSel)
-        {
-            BlockEntityAutoloom beQuern = world.BlockAccessor.GetBlockEntity(blockSel.Position) as BlockEntityAutoloom;
-            if (beQuern != null)
-            {
-                beQuern.SetPlayerGrinding(byPlayer, false);
-            }
-
-        }
-
-        public override bool OnBlockInteractCancel(float secondsUsed, IWorldAccessor world, IPlayer byPlayer, BlockSelection blockSel, EnumItemUseCancelReason cancelReason)
-        {
-            BlockEntityAutoloom beQuern = world.BlockAccessor.GetBlockEntity(blockSel.Position) as BlockEntityAutoloom;
-            if (beQuern != null)
-            {
-                beQuern.SetPlayerGrinding(byPlayer, false);
-            }
-
-
-            return true;
-        }
 
         public override WorldInteraction[] GetPlacedBlockInteractionHelp(IWorldAccessor world, BlockSelection selection, IPlayer forPlayer)
         {
-            if (selection.SelectionBoxIndex == 0)
+            return new WorldInteraction[]
             {
-                return new WorldInteraction[] {
-                    new WorldInteraction()
-                    {
-                        ActionLangCode = "blockhelp-quern-addremoveitems",
-                        MouseButton = EnumMouseButton.Right
-                    }
-                }.Append(base.GetPlacedBlockInteractionHelp(world, selection, forPlayer));
-            }
-            else
-            {
-                return new WorldInteraction[] {
-                    new WorldInteraction()
-                    {
-                        ActionLangCode = "blockhelp-quern-grind",
-                        MouseButton = EnumMouseButton.Right,
-                        ShouldApply = (wi, bs, es) => {
-                            BlockEntityAutoloom beQuern = world.BlockAccessor.GetBlockEntity(bs.Position) as BlockEntityAutoloom;
-                            return beQuern != null && beQuern.CanGrind();
-                        }
-                    }
-                }.Append(base.GetPlacedBlockInteractionHelp(world, selection, forPlayer));
-            }
+                new WorldInteraction
+                {
+                    ActionLangCode = "blockhelp-quern-addremoveitems",
+                    MouseButton = EnumMouseButton.Right
+                }
+            }.Append(base.GetPlacedBlockInteractionHelp(world, selection, forPlayer));
         }
+
+        public override bool HasMechPowerConnectorAt(IWorldAccessor world, BlockPos pos, BlockFacing face)
+        {
+            Block block = world.BlockAccessor.GetBlock(pos);
+
+            float rotY = block.Shape?.rotateY ?? 0f;
+            int rotSteps = ((int)Math.Round(rotY / 90f) % 4 + 4) % 4;
+            BlockFacing[] horizontals = BlockFacing.HORIZONTALS;
+            int index = Array.IndexOf(horizontals, BlockFacing.WEST);
+            int rotatedIndex = (index + rotSteps) % 4;
+
+            // Flip for east/west rotations
+            if (rotSteps == 1 || rotSteps == 3)
+            {
+                rotatedIndex = (rotatedIndex + 2) % 4;
+            }
+
+            BlockFacing connector = horizontals[rotatedIndex];
+
+            return face == connector;
+        }
+
 
         public override void DidConnectAt(IWorldAccessor world, BlockPos pos, BlockFacing face)
         {
 
         }
-
-        public override bool HasMechPowerConnectorAt(IWorldAccessor world, BlockPos pos, BlockFacing face)
-        {
-            return face == BlockFacing.UP || face == BlockFacing.DOWN;
-        }
-
-        public override void OnEntityCollide(IWorldAccessor world, Entity entity, BlockPos pos, BlockFacing facing, Vec3d collideSpeed, bool isImpact)
-        {
-            base.OnEntityCollide(world, entity, pos, facing, collideSpeed, isImpact);
-
-            if (facing == BlockFacing.UP)
-            {
-
-                if (entity.World.Side == EnumAppSide.Server)
-                {
-                    float frameTime = GlobalConstants.PhysicsFrameTime;
-                    var mpc = GetBEBehavior<BEBehaviorMPConsumer>(pos);
-                    if (mpc != null)
-                    {
-                        entity.SidedPos.Yaw += frameTime * mpc.TrueSpeed * 2.5f * (mpc.isRotationReversed() ? -1 : 1);
-                    }
-                }
-                else
-                {
-                    float frameTime = GlobalConstants.PhysicsFrameTime;
-                    var mpc = GetBEBehavior<BEBehaviorMPConsumer>(pos);
-                    var capi = api as ICoreClientAPI;
-                    if (capi.World.Player.Entity.EntityId == entity.EntityId)
-                    {
-                        var sign = mpc.isRotationReversed() ? -1 : 1;
-                        if (capi.World.Player.CameraMode != EnumCameraMode.Overhead)
-                        {
-                            capi.Input.MouseYaw += frameTime * mpc.TrueSpeed * 2.5f * sign;
-                        }
-                        capi.World.Player.Entity.BodyYaw += frameTime * mpc.TrueSpeed * 2.5f * sign;
-                        capi.World.Player.Entity.WalkYaw += frameTime * mpc.TrueSpeed * 2.5f * sign;
-                        capi.World.Player.Entity.Pos.Yaw += frameTime * mpc.TrueSpeed * 2.5f * sign;
-                    }
-                }
-            }
-        }
-
     }
 }
